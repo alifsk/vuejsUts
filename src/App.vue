608a11a33e8fa924f22706c3e3781eaa4679a90a
ask02 <alifkurniawan292@gmail.com>
2020-03-21 22:11:44 +0700

    Page Ecommerce Uts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <link rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            crossorigin="anonymous">
+        <div>
+            <b-navbar class="navbar" id="nav" toggleable="lg" fixed="top">
+                <b-navbar-brand  class="navbar-brand m-auto" href="#home">
+                    <img src="./assets/images/logo.png" alt="logo" class="logo">
+                </b-navbar-brand>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-collapse id="nav-collapse" class="nav-collapse" is-nav>
+                    <b-navbar-nav class="mr-auto">
+                        <b-nav-item class="nav-item" href="/" right><b>Home</b></b-nav-item>
+                        <b-nav-item class="nav-item" href="" right><b>About Us</b></b-nav-item>
+                        <b-nav-item class="nav-item" href="product" right><b>POD</b></b-nav-item>
+                        <b-nav-item class="nav-item" href="" right><b>MOD</b></b-nav-item>
+                        <b-nav-item class="nav-item" href="" right><b>Liquid</b></b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+                <b-collapse id="nav-collapse" class="nav-collapse" is-nav>
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item class="nav-item" href="" right>
+                            <i class="fas fa-search"></i>
+                        </b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>
+                            <i class="fas fa-user"></i>
+                        </b-nav-item>
+                        <b-nav-item class="nav-item" href="" right>
+                            <i class="fas fa-shopping-cart"></i>
+                        </b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+        <router-view />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('scroll', () => {
+        const navbar = document.getElementById('nav');
+        // eslint-disable-next-line camelcase
+        const nav_class = navbar.classList;
+        if (document.documentElement.scrollTop >= 150) {
+          if (nav_class.contains('shrink') === false) {
+            nav_class.toggle('shrink');
+          }
+        } else if (nav_class.contains('shrink') === true) {
+          nav_class.toggle('shrink');
+        }
+      });
+    });
+  },
+};
 </script>
-
 <style>
+
+html {
+    scroll-behavior: smooth;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: none;
+    color: #2c3e50;
+    margin-top: 0;
+}
+.navbar {
+    background-color: transparent;
+    transition: 0.3s ease;
+}
+nav.shrink {
+    background-color: white;
+    box-shadow: 0 1px 4px 0 rgb(0, 0, 0, 5);
+}
+.nav-item {
+    padding-left: 5px;
+    padding-right: 5px;
+    color: black;
+}
+.logo {
+    width: 140px;
+    opacity: 0.5;
+    padding: 20px;
 }
 </style>
---
+++ src/views/index.vue
@@ -0,0 +1,326 @@
+<template>
+    <div class="home">
+        <div>
+            <b-carousel id="carousel-1" v-model="slide"
+                :interval="4000"
+                controls
+                indicators
+                img-width="1024"
+                img-height="480"
+            >
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide2.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide1.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide3.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide4.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide5.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide6.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+                <b-carousel-slide>
+                    <template v-slot:img>
+                    <img
+                        class="d-block img-fluid w-100 img-banner"
+                        src="../assets/images/slide7.jpg"
+                        alt="image slot"
+                    >
+                    </template>
+                </b-carousel-slide>
+            </b-carousel>
+        </div>
+        <section class="product text-center mt-5">
+            <b-tabs align="center" pills no-key-nav>
+                <b-tab class="category-item" title="All">
+                    <div class="container-fluid container-product">
+                        <div class="row row-product">
+                            <div class="col-md-3" v-for="(data, index) in products" :key="index">
+                                <div class="product-item">
+                                    <div class="product-img text-center">
+                                        <img
+                                            :src="require(`../assets/images/${data.foto}`)"
+                                            class="img-product"
+                                        >
+                                    </div>
+                                    <div class="product-detail text-center">
+                                        <h5><b>{{data.nama}}</b></h5>
+                                        <h6>{{data.harga}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </b-tab>
+                <b-tab class="category-item" title="Pod">
+                    <div class="container-fluid container-product">
+                        <div class="row row-product">
+                            <div class="col-md-3" v-for="(data, index) in pod" :key="index">
+                                <div class="product-item">
+                                    <div class="product-img text-center">
+                                        <img
+                                            :src="require(`../assets/images/${data.foto}`)"
+                                            class="img-product"
+                                        >
+                                    </div>
+                                    <div class="product-detail text-center">
+                                        <h5><b>{{data.nama}}</b></h5>
+                                        <h6>{{data.harga}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </b-tab>
+                <b-tab class="category-item" title="Mod">
+                    <div class="container-fluid container-product">
+                        <div class="row row-product">
+                            <div class="col-md-3" v-for="(data, index) in mod" :key="index">
+                                <div class="product-item">
+                                    <div class="product-img text-center">
+                                        <img
+                                            :src="require(`../assets/images/${data.foto}`)"
+                                            class="img-product"
+                                        >
+                                    </div>
+                                    <div class="product-detail text-center">
+                                        <h5><b>{{data.nama}}</b></h5>
+                                        <h6>{{data.harga}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </b-tab>
+                <b-tab class="category-item" title="Liquid">
+                    <div class="container-fluid container-product">
+                        <div class="row row-product">
+                            <div class="col-md-3" v-for="(data, index) in liquid" :key="index">
+                                <div class="product-item">
+                                    <div class="product-img text-center">
+                                        <img
+                                            :src="require(`../assets/images/${data.foto}`)"
+                                            class="img-product"
+                                        >
+                                    </div>
+                                    <div class="product-detail text-center">
+                                        <h5><b>{{data.nama}}</b></h5>
+                                        <h6>{{data.harga}}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </b-tab>
+            </b-tabs>
+            <div class="container-fluid">
+                <div class="row text-center">
+                    <div class="col-md-12 text-center">
+                        <button type="button" class="btn btn-load-more mb-5">LOAD MORE</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="newsletter">
+            <div class="container-fluid">
+                <div class="row m-5">
+                    <div class="col-md-12 text-center">
+                        <h4><b>Newsletter</b></h4>
+                        <p>Get the weekly promotions after you subscribe below</p>
+                        <div class="input-group mb-3">
+                            <input
+                                type="text"
+                                class="form-control input-subscribe"
+                                placeholder="Enter your email address"
+                                aria-label="email address"
+                                aria-describedby="button-subscribe"
+                            >
+                            <div class="input-group-append">
+                                <button
+                                    class="btn btn-outline-secondary btn-subscribe"
+                                    type="button"
+                                    id="button-subscribe">
+                                    <b>Subscribe</b>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contact">
+            <footer class="footer-contact">
+                <div class="row row-contact">
+                    <div class="col-md-4">
+                    <img src="../assets/images/logo.png" alt="logo" class="logo-footer img-fluid">
+                        <br/>
+                        <p class="link-footer">Vape Of Indonesia</p>
+                    </div>
+                    <div class="col-md-4 pl-4">
+                        <p><b>Quick Links</b></p>
+                        <div class="row">
+                            <div class="col-6">
+                                <a href="product" class="link-footer"><p>MOD</p></a>
+                                <a href="#product" class="link-footer"><p>POD</p></a>
+                                <a href="#product" class="link-footer"><p>Liquid</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <p><b>Contact Us</b></p>
+                        <p class="link-footer">
+                    Email : <a href="mailto:alifkurniawan292@gmail.com" class="link-footer">
+                        alifkurniawan292@gmail.com</a>
+                        </p>
+                        <p class="link-footer">
+                    WhatsApp: <a href="tel:+6285646023469" class="link-footer">+6285646023469</a>
+                        </p>
+                        <p class="link-footer">Instagram : <a href="https://www.instagram.com/alifsk_" class="link-footer">@alifsk_</a></p>
+                        <p class="link-footer">Twitter : <a href="https://twitter.com/Alifsk_" class="link-footer">@Alifsk_</a></p>
+                    </div>
+                </div>
+                <br/><br/>
+                <p class="text-center link-footer">
+                    Copyright &copy;2020 Vape of Indonesia. All Rights Reserved
+                    <br/>
+                    Created by Alif Setyakurniawan
+                </p>
+            </footer>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      products: [],
+      pod: [],
+      mod: [],
+      liquid: [],
+    };
+  },
+  async created() {
+    const BASEURI = 'https://my-json-server.typicode.com/alifsk/dbuts/db';
+    axios.get(BASEURI).then((result) => {
+      this.products = result.data.products;
+      this.pod = result.data.pod;
+      this.mod = result.data.mod;
+      this.liquid = result.data.liquid;
+    });
+  },
+};
+</script>
+
+<style scoped>
+@import url('http://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css');
+
+.title {
+    text-align: left;
+    top: 50%;
+}
+.row-product {
+    padding: 50px;
+}
+.tab-category {
+    border-bottom: 1px solid black;
+}
+.product-item {
+    transition: 0.3s;
+}
+.product-img {
+    padding: 20px;
+    background-color: #E6E6E6;
+}
+.img-product {
+    height: 250px;
+}
+.product-detail {
+    padding: 50px;
+}
+.btn-load-more {
+    padding: 15px 30px;
+    background-color: #141414;
+    border-radius: 0;
+    color: #E6E6E6; 
+    transition: 0.3s ease;
+}
+.btn-load-more:hover {
+    background-color: transparent;
+    color: #141414; 
+    border: 1px solid #141414;
+}
+.input-subscribe {
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid #b8b8b8;
+    background-color: transparent;
+}
+.btn-subscribe {
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid #b8b8b8;
+    background-color: transparent;
+}
+.btn-subscribe:hover {
+    background-color: #141414;
+}
+.footer-contact {
+    padding: 40px;
+    background-color: black;
+    color: white;
+}
+.logo-footer {
+    width: 200px;
+    padding-bottom: 10px;
+}
+.link-footer {
+    text-decoration: none;
+    color: dodgerblue;
+}
+</style>
